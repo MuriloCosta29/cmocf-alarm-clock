628b364a736321560b2cca2e0a60b5fdf03e5562
murilo <murilojagger@gmail.com>
2026-09-16 20:23:34 -0300

    feat(threads): add wakeup_tick in struct thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -93,6 +93,9 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    /* Absolute Tick where the blocked thread in timer_sleep should wake up */
+    int64_t wakeup_tick;
+
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
